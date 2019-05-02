8c2f305bb7606d383c92af99120054207ef08b96
Liu-YT <1039868176@qq.com>
2019-05-02 17:29:49 +0800

    use json

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 # 配置数据库的地址
 # window 10
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost:3306/mydb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:2craWbasil@localhost:3306/mydb'
 
 # centos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/mydb'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/mydb'
 
 # 跟踪数据库的修改，不建议开启
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -39,7 +39,7 @@ db.create_all()
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    code = request.form.to_dict()['code']
+    code = request.json['code']
     res_wechat = requests.get("https://api.weixin.qq.com/sns/jscode2session?appid=wx25915d3c4f6a78f3&secret=133e74afeca06c60a597cf3b694a6c87&js_code="+code+ "&grant_type=authorization_code").json()
     res_jqdata = requests.post("https://dataapi.joinquant.com/apis", data=json.dumps({
         "method": "get_token",
@@ -47,38 +47,54 @@ def login():
         "pwd": "401698",  # Password为聚宽官网登录密码，新申请用户默认为手机号后6位
     }))
     res = {
-        "openid": res_wechat['openid'],
-        "token": res_jqdata.text
+        "openid": '',
+        "token": '',
+        "msg": ''
     }
+
+    if res_wechat['errcode'] == 0:
+        res["openid"] = res_wechat["openid"]
+        res["token"] = res_jqdata.text
+    else:
+        res["msg"] = res_wechat["errmsg"]
+
     return json.dumps(res)
 
 @app.route('/api/add', methods=['POST'])
 def add_stock():
-    form = request.form.to_dict()
-    res = {'user': form['user'], 'stocks': []}
-    query_result = User.query.filter_by(id=form['user']).first()
-    stocks = get_stocks_list(query_result.stocks)
-    if form['stock'] not in stocks:
-        stocks.append(form['stock'])
-        query_result.stocks = get_stocks_str(stocks)
-        db.session.add(query_result)
-        db.session.commit()
-    res['stocks'] = stocks
+    try:
+        form = request.json
+        res = {'user': form['user'], 'stocks': []}
+        query_result = User.query.filter_by(id=form['user']).first()
+        stocks = get_stocks_list(query_result.stocks)
+        if form['stock'] not in stocks:
+            stocks.append(form['stock'])
+            query_result.stocks = get_stocks_str(stocks)
+            db.session.add(query_result)
+            db.session.commit()
+        res['stocks'] = stocks
+    except BaseException as e:
+        print(e)
+        res["msg"] = "Incorrect parameter"
     return json.dumps(res, ensure_ascii=False)
 
 
 @app.route('/api/remove', methods=['POST'])
 def remove_stock():
-    form = request.form.to_dict()
-    res = {'user': form['user'], 'stocks': []}
-    query_result = User.query.filter_by(id=form['user']).first()
-    stocks = get_stocks_list(query_result.stocks)
-    if form['stock'] in stocks:
-        stocks.remove(form['stock'])
-        query_result.stocks = get_stocks_str(stocks)
-        db.session.add(query_result)
-        db.session.commit()
-    res['stocks'] = stocks
+    form = request.json
+    res = {'user': form['user'], 'stocks': [], 'msg': ''}
+    try:
+        query_result = User.query.filter_by(id=form['user']).first()
+        stocks = get_stocks_list(query_result.stocks)
+        if form['stock'] in stocks:
+            stocks.remove(form['stock'])
+            query_result.stocks = get_stocks_str(stocks)
+            db.session.add(query_result)
+            db.session.commit()
+        res['stocks'] = stocks
+    except BaseException as e:
+        print(e)
+        res["msg"] = "Incorrect parameter"
     return json.dumps(res, ensure_ascii=False)
 
 @app.route('/api/news', methods=['GET'])
